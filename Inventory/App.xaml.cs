de9407846c06d46486b57b28ac3f4262dec3f98c
Edge Gonzaga <144799619+PhsMessi@users.noreply.github.com>
2026-03-30 12:00:37 +0800

    added project key

--- Inventory/App.xaml.cs
+++ Inventory/App.xaml.cs
@@ -10,6 +10,6 @@ namespace Inventory
 
         // Supabase credentials
         public static string SupabaseUrl = "https://ukhousqmmypaqjqdegbt.supabase.co";
-        public static string SupabaseKey = "your-anon-key-here";
+        public static string SupabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVraG91c3FtbXlwYXFqcWRlZ2J0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ4MjQ2NzgsImV4cCI6MjA5MDQwMDY3OH0.N0MiLiJrAFlotusHG0vJ5qsxIiSzQw6Ibf3FpkgPswI";
     }
 }
